70eb20ed1bd08cd93b8e9aeb9fa8e311b547d5cf
oren <get.oren@gmail.com>
2012-10-28 22:31:31 -0400

    add build, web components and visual layout

---
+++ build.dart
@@ -0,0 +1,8 @@
+#!/usr/bin/env dart
+
+import 'dart:io';
+import 'package:web_components/component_build.dart';
+
+void main() {
+  build(new Options().arguments, ['web/main.html']);
+}
---
+++ web/main.dart
@@ -0,0 +1,6 @@
+String curTime;
+
+main() {
+  var today = new Date.now();
+  curTime = '${today.hour}:${today.minute} XM';
+}
