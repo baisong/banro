d9a21ae6d9fa9e0100f941770263cbb77e404042
oren <get.oren@gmail.com>
2012-10-30 23:42:41 -0400

    first add shuffle for tiles and chits (probabilities)

--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,12 @@
+import 'model.dart';
+
 String curTime;
+String boardString;
 
 main() {
+  var game = new Game();
+  boardString = game.printBoard();
+
   var today = new Date.now();
   curTime = '${today.hour}:${today.minute} XM';
 }
