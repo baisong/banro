46411bd868f11250b5a36fd472967faa07a60dcb
oren <get.oren@gmail.com>
2012-11-06 19:25:23 -0500

    comment and refactor

--- web/model.dart
+++ web/model.dart
@@ -2,23 +2,29 @@ library model;
 import 'dart:math';
 import 'board.dart';
 
+/**
+ * Represents one game from start to finish.
+ */
 class Game {
 
   List<String> resources;
   List<int> probs;
   List<Tile> tiles;
 
+  /**
+   * Prints a text-only visualization of the game board.
+   */
   String printBoard() {
-
     String board = blankBoard();
-
     for (Tile tile in tiles) {
       board = board.replaceFirst('###', '$tile');
     }
-
     return board;
   }
 
+  /**
+   * Sets up a new random board.
+   */
   Game.initBoard() {
     this.tiles = [];
     this.resources = shuffle(['W','W','W','W','L','L','L','L','S','S','S','S','I','I','I','B','B','B','X']);
@@ -42,13 +48,18 @@ class Game {
   }
 }
 
+/**
+ * Represents a generic game piece, generalizes attibutes & display
+ */
 class Piece {
   String textSymbol;
   int position;
   String id;
-  //Piece(this.textSymbol);
 }
 
+/**
+ * Represents an individual hex-tile
+ */
 class Tile extends Piece {
   List<String> edges;
   List<String> nodes;
@@ -58,21 +69,33 @@ class Tile extends Piece {
   Tile(String resource, int probability) {
     this.resource = resource;
     this.probability = probability;
+
+    // @todo instead return one of: '', 'W', 'WW', 'WWW','WWWW', 'WWWWW'
     String buffer = probability < 10 ? '0' : '';
     this.textSymbol = '${resource}${buffer}${probability}';
   }
+
   String toString() => '${textSymbol}';
 }
 
+/**
+ * Represents a single side of a hex, where roads are built.
+ */
 class Edge extends Piece {
   List<String> nodes;
+  List<String> tiles;
 }
 
+/**
+ * Represents a single corner of three edges, where cities are built.
+ */
 class Node extends Piece {
   List<String> edges;
 }
 
-// Shuffle a list randomly.
+/**
+ * Shuffles a list randomly.
+ */
 List shuffle(List list) {
   int len = list.length - 1;
   for (int i = len; i >= 0; i--) {
